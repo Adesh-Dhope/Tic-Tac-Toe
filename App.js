0ff1c3caf2e6a50a1211616ec2b983d3fb2bea7d
Adesh-Dhope <146838880+Adesh-Dhope@users.noreply.github.com>
2024-01-25 12:50:17 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Board from './Game/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      
+      <Board />
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
